807a1cfd2d66be75b158ae4a852177bbca2639d1
vaxicy <huangzero2004@gmail.com>
2026-09-23 19:21:01 +0800

    修复英语陪练弹窗输入框被消息区挤出裁剪的问题

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pink-workbench-v100';
+const CACHE_NAME = 'pink-workbench-v101';
 
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));

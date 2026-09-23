a79ab869593c83a140c2449a09bfc296bfee62f5
vaxicy <huangzero2004@gmail.com>
2026-09-23 19:17:25 +0800

    英语tab行布局优化+AI陪练可自定义名字

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pink-workbench-v99';
+const CACHE_NAME = 'pink-workbench-v100';
 
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));

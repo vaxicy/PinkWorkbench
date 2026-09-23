2d98c407b58852abc0b20da484ff085a42bc4e5e
vaxicy <huangzero2004@gmail.com>
2026-09-23 20:05:58 +0800

    宠物卡片聊天入口改名：记录→聊天

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pink-workbench-v104';
+const CACHE_NAME = 'pink-workbench-v105';
 
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));

d2b6b23d32cfbc980f6f6a8ec390297e168b31d2
vaxicy <huangzero2004@gmail.com>
2026-09-26 02:04:19 +0800

    塔罗: 换用白底金线月相牌背(v115)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pink-workbench-v114';
+const CACHE_NAME = 'pink-workbench-v115';
 
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));

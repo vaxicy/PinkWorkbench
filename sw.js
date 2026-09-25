71cf04af5b5a27f384508ed91e749a60f87748a7
vaxicy <huangzero2004@gmail.com>
2026-09-26 02:13:46 +0800

    塔罗圆弧: 去紫光带+金线描边+疏排+弧长自适应布局(v117)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pink-workbench-v116';
+const CACHE_NAME = 'pink-workbench-v117';
 
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));

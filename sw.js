6dfd516f8d159003dcc0b2b95afd42e80176f7c3
vaxicy <huangzero2004@gmail.com>
2026-09-26 02:16:06 +0800

    运势改10分制(下限5分)+评级门槛调整+缓存版本v2(v118)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pink-workbench-v117';
+const CACHE_NAME = 'pink-workbench-v118';
 
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
